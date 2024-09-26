9a7cc6ab034f346f0beba2f2297a1986c96e9bd5
Elyx <151476172+elyxdev@users.noreply.github.com>
2024-09-26 15:37:33 -0500

    Made 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'layouts/header.php';
+?>
+<div class="mt-[45%] h-fit w-fit text-center">
+    <h1 class="text-4xl font-bold">Ey, parece que te has perdido...<br><a class="text-green-700" href="<?php echo $r_base?>">¿Volver al inicio?</a></h1>
+</div>
+<?php
+require_once 'layouts/footer.php';
+?>
